d17470f1d68b99f8b2fba73469d6f078ba8eda91
eduardo_dev_sollar <eduardo.pereira_dev@soollar.com.br>
2025-04-21 14:17:37 -0300

    fix: correction page task

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -19,7 +19,7 @@ const TaskPage = () => {
   return (
     <div className="h-screen w-screen bg-slate-500 p-6">
       {/* Usando diretivas para mostrar os valores dos parametros da url */}
-      <div className="w-[500px] space-y-4">
+      <div className="w-[500px] mx-auto space-y-4">
         <div className="flex justify-center relative mb-6">
           <button onClick={() => navigate(-1)} className="absolute left-0 top-0 bottom-0 text-slate-100 cursor-pointer">
             <ChevronLeft />
